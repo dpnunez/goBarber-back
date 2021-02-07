446ba68d6d94c988652dae52dcb8103cdc56d092
Daniel Pôrto Núñez <porto@MacBook-Pro-de-Daniel.local>
2021-02-06 21:45:58 -0300

    feat: add relation between provider's and appointment'

--- src/database/migrations/1612462102084-CreateAppointment.ts
+++ src/database/migrations/1612462102084-CreateAppointment.ts
@@ -9,7 +9,7 @@ export default class CreateAppointment1612462102084
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
---
+++ src/database/migrations/1612657961012-updateProviderColumnToProviderId.ts
@@ -0,0 +1,49 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class updateProviderColumnToProviderId1612657961012
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // remove the column
+    await queryRunner.dropColumn('appointments', 'provider');
+
+    // Create new column
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    // Add relationship
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+    await queryRunner.dropColumn('appointments', 'provider_id');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      }),
+    );
+  }
+}
