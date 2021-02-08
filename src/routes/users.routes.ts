e41a059407534f3738447ba07db15b8ccb5a256c
Daniel Pôrto Núñez <porto@MacBook-Pro-de-Daniel.local>
2021-02-08 19:16:26 -0300

    feat: add hashed pass

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,17 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import { User } from '../models';
 import { CreateUserService } from '../services';
 
 const usersRouter = Router();
+
+usersRouter.get('/', async (_, res) => {
+  const usersRepository = getRepository(User);
+  const users = await usersRepository.find();
+
+  return res.json(users);
+});
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -15,8 +25,15 @@ usersRouter.post('/', async (req, res) => {
       password,
     });
 
-    return user;
-    return res.send();
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return res.json(userWithoutPassword);
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
